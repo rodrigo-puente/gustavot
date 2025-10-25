39a57fe11b251013c10475915b201d4f69647363
Rodrigo Puente <hello@rodrigopuente.com>
2025-10-25 15:07:08 -0600

    Fix secret key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Explicitly use SECRET_KEY_BASE from environment if available
+  config.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"].present?
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
